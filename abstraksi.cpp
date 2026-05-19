2c6c4c0623219041d113f515d56350561dccd739
fer4rie <firapuspita2106@gmail.com>
2026-05-19 16:52:27 +0700

    menambah method setXY untuk mengisi nilai atribut x dan y

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -4,3 +4,10 @@ using namespace std;
 class AbstraksiKlas {
 private:
     string x, y;
+public:
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
